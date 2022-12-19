f760fc76d8466fc2c4cf3a718bb2f018204977d0
academician <academician@gmail.com>
2022-12-19 20:46:04 +0000

    Clean up devcontainer

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -6,7 +6,6 @@ apt-get install -y \
   gnupg2 \
   jq \
   sudo \
-  zsh \
   vim \
   build-essential \
   openssl
@@ -17,18 +16,12 @@ apt-get install -y pkg-config
 ## Install rustup and common components
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 export PATH=/root/.cargo/bin:$PATH
-rustup install nightly
+rustup install stable
+#rustup install nightly
 rustup component add rustfmt
-rustup component add rustfmt --toolchain nightly
+#rustup component add rustfmt --toolchain nightly
 rustup component add clippy 
-rustup component add clippy --toolchain nightly
+#rustup component add clippy --toolchain nightly
 
 cargo install cargo-expand
 cargo install cargo-edit
-
-## setup and install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-cp -R /root/.oh-my-zsh /home/$USERNAME
-cp /root/.zshrc /home/$USERNAME
-sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
-chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
